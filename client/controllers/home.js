0184937aa476794213f57e05b3c3ab5153913655
Thomas Babu <tommy.developer@gmail.com>
2014-12-19 18:15:01 +0800

    Added home controller

---
+++ client/controllers/home.js
@@ -0,0 +1,11 @@
+
+angular.module('Instagram')
+  .controller('HomeCtrl', function($scope) {
+    $scope.isAuthenticated = function() {
+      // Check if logged in
+    };
+
+    $scope.linkInstagram = function() {
+      // connect email account with Instagram
+    };
+  })
