f9ff6917f72a5dc2eff1078be1414a09d7899ca8
Thomas Babu <tommy.developer@gmail.com>
2014-12-19 17:35:44 +0800

    added the route for all the links

--- client/app.js
+++ client/app.js
@@ -1,4 +1,22 @@
 angular.module('Instagram', ['ngRoute', 'ngMessages'])
   .config( function($routeProvider) {
 
+      $routeProvider
+        .when('/', {
+          templateUrl : 'views/home.html',
+          controller : 'HomeCtrl'
+        })
+        .when('/login', {
+          templateUrl : 'views/login.html',
+          controller : 'LoginCtrl',
+        })
+        .when('/signup', {
+          templateUrl : 'views/signup.html',
+          controller : 'SignupCtrl'
+        })
+        .when('photo/:id', {
+          templateUrl : 'views/details.html',
+          controller : 'DetailCtrl'
+        })
+        .otherwise('/');
   });
