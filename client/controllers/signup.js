8ca506ad2dc7c0163876104bc7c485f129254f37
Thomas Babu <tommy.developer@gmail.com>
2014-12-21 00:03:32 +0800

    created signup controller

---
+++ client/controllers/signup.js
@@ -0,0 +1,15 @@
+
+angular.module('Instagram')
+	.controller('SignupCtrl', function($scope, $auth){
+		$scope.signup = function() {
+			var user = {
+				email : $scope.email,
+				password : $scope.password
+			};
+
+			$auth.signup(user)
+				.catch(function(response){
+					console.log (response.error);
+				});
+		};
+	});
