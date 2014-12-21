fb89f4e7f97560e14e40a1a87341ccfdbb90be7b
Thomas Babu <tommy.developer@gmail.com>
2014-12-21 18:21:32 +0800

    updated JWT token

--- server/config.js
+++ server/config.js
@@ -1,4 +1,5 @@
 module.exports = {
 	db: 'localhost',
-	clientSecret: process.env.clientSecret || 'a24fb4b292b44655b920316245b62154'
+	clientSecret: process.env.clientSecret || 'a24fb4b292b44655b920316245b62154',
+	tokenSecret : process.env.tokenSecret || 'IamhardtoGue$$';
 };
