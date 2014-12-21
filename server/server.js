4eee246a395a3201d3cead72f6d3bf3b55769cce
Thomas Babu <tommy.developer@gmail.com>
2014-12-21 18:19:24 +0800

    fixed the typos on server file

--- server/server.js
+++ server/server.js
@@ -7,16 +7,16 @@ var jwt = require('jwt-simple');
 var moment = require('moment');
 var mongoose = require('mongoose');
 var path = require('path');
-var request = request('request');
+var request = require('request');
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use( cors() );
 app.use( bodyParser.json() );
-app.use( bodyParser.urlencodd( { extended : false }));
+app.use( bodyParser.urlencoded( { extended : false }));
 app.use ( express.static (path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), function() {
-	console.log ( 'Express server listening on port' + app.get('port'));
+	console.log ( 'Express server listening on port ' + app.get('port'));
 });
