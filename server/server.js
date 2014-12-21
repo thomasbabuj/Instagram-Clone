6d253375590f116f20d667b140930ce876cdc391
Thomas Babu <tommy.developer@gmail.com>
2014-12-21 18:57:15 +0800

    added createtoken and isAuthenticated middleware functions

--- server/server.js
+++ server/server.js
@@ -34,3 +34,51 @@ app.use ( express.static (path.join(__dirname, 'public')));
 app.listen(app.get('port'), function() {
 	console.log ( 'Express server listening on port ' + app.get('port'));
 });
+
+/*
+|------------------------------------------------------------------
+| Generate JSON Web Token 
+|------------------------------------------------------------------
+*/
+
+function createToken(user) {
+	var payload = {
+		exp : moment().add(14, 'days').unix(),
+		iat : moment().unix(),
+		sub: user._id
+	};
+
+	return jwt.encode(payload, config.tokenSecret);
+}
+
+/*
+|------------------------------------------------------------------
+| Login Required Middleware
+|------------------------------------------------------------------
+*/
+
+function isAuthenticated(req, res, next) {
+	if ( !(req.headers && req.headers.authorization)) {
+		return res.status(400).send({ message : 'You did not provide a JSON Web Token in the Authorization header.' });
+	}
+
+	var header = req.headers.authorization.split(' ');
+	var token = header[1];
+	var payload = jwt.decode(token, config.tokenSecret);
+	var now = moment().unix();
+
+	if ( now > payload.exp )
+	{
+		return res.status(401).send({ message: 'Token has expired.'});
+	}
+
+	User.findById(payload.sub, function(err, user){
+		if ( !user )
+		{
+			return res.status(400).send({ 'message': 'User no longer exists.'});
+		}
+
+		req.user = user;
+		next();
+	});
+}
