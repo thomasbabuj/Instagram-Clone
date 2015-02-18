26501342913b531c1832ccfb6a3b7abb34fa9910
Thomas Babu <tommy.developer@gmail.com>
2015-02-18 10:52:25 +0800

    fixed the class name typo issue

---
+++ client/controllers/home.js
@@ -0,0 +1,14 @@
+/* Home Controller */
+
+angular.module('Instagram')
+  .controller('HomeCtrl', function($scope){
+
+    $scope.isAuthenticated = function() {
+      // Check if user logged in
+    };
+
+    $scope.linkInstagram = function() {
+      // Connect email account with Instagram
+    };
+
+  });
