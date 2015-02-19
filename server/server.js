6bdd9bf7cb93281b6bee9971cd516cb54a1d242b
Thomas Babu <tommy.developer@gmail.com>
2015-02-19 18:18:16 +0800

    Created a new User model and connect to MongoDB

--- server/server.js
+++ server/server.js
@@ -12,6 +12,18 @@ var request = require('request');
 
 var config = require('./config');
 
+var User = mongoose.model('User', new mongoose.Schema({
+	instagramId : { type : String, index : true },
+	email : { type : String, unique : true, lowercase: true },
+	password : { type : String, select : false },
+	username : String,
+	fullName : String,
+	picture : String,
+	accessToken : String
+}));
+
+mongoose.connect(config.db);
+
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
