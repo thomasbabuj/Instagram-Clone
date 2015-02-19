83e2bedad6171b032abbae90ce74bd147cf5af56
Thomas Babu <tommy.developer@gmail.com>
2015-02-19 18:09:34 +0800

    created config file and included in express js file

---
+++ server/config.js
@@ -0,0 +1,6 @@
+/* Common Config file */
+
+module.exports = {
+	db : process.env.db || 'localhost',
+	tokenSecret : process.env.tokenSecret || '983#bn+(dv46ep!e*s4*v+=grwb4mix(r7qy9ls$fm^wq^)sw&'
+};
