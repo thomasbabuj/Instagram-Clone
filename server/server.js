4aa8775d2e39fc3e5c403a81e51c703aff1870ed
Thomas Babu <tommy.developer@gmail.com>
2015-02-19 17:53:45 +0800

    installed requied node moduels

---
+++ server/server.js
@@ -0,0 +1,23 @@
+/* Express entry point */
+
+var bcrypt = require('bcryptjs');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var express = require('express');
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var mongoose = require('mongoose');
+var path = require('path');
+var request = require('request');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), function(){
+	console.log('Express server listening on port' + app.get('port'));
+});
